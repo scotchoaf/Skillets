4caf179d14c6620e6f0fb67e18a322dba5700792
Nathan <nembery@gmail.com>
2019-09-17 15:13:12 -0400

    example script to generate snippets

---
+++ python/generate_snippets/generate_skillet_snippets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+# This example uses ENV variables to gather input from the user
+# The .meta-cnc file defines 3 input variables:
+# USERNAME, PASSWORD, SECRET
+# This script shows how to obtain the values entered from the user
+#
+import json
+import os
+import sys
+
+from skilletlib.exceptions import LoginException
+from skilletlib.exceptions import SkilletLoaderException
+from skilletlib.panoply import Panoply
+
+# each variable will be present in the environ dict on the 'os' module
+username = os.environ.get('TARGET_USERNAME', 'admin')
+password = os.environ.get('TARGET_PASSWORD', 'admin')
+ip = os.environ.get('TARGET_IP', '')
+
+snippets = list()
+
+try:
+    device = Panoply(hostname=ip, api_username=username, api_password=password)
+    snippets = device.generate_skillet()
+    print(json.dumps(snippets))
+    sys.exit(0)
+
+except SkilletLoaderException as se:
+    print('[]')
+    sys.exit(1)
+except LoginException as le:
+    print('[]')
+    sys.exit(1)
